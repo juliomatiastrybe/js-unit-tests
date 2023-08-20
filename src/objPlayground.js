472d0eae05205eddc03aa22f84dc5ab2683c22cb
Julio Matias <juliocesark2@hotmail.com>
2023-08-20 15:09:29 -0300

    requesito 7, comentando o console.log

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -61,5 +61,5 @@ const arrayGenerator = (type, object) => {
 // console.log(arrayGenerator('key', { sum: 3, mult: 2, div: 0, sub: -1 }));
 // console.log(arrayGenerator('values', { sum: 3, mult: 2, div: 0, sub: -1 }));
 // console.log(arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }));
-console.log(arrayGenerator('keys', { sum: 4, mult: 3, div: 0, sub: -2 }));
+// console.log(arrayGenerator('keys', { sum: 4, mult: 3, div: 0, sub: -2 }));
 module.exports = { calculator, arrayGenerator };
