92db77c9462b33962da7b1b302cc6173b424c1ca
Julio Matias <juliocesark2@hotmail.com>
2023-08-20 19:56:23 -0300

    requesito 10.3 ok

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,11 +1,18 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 
-const createMenu = () => {
+const createMenu = (menu) => {
     const MenuObject = {
-        fetchMenu: () => {},
+        fetchMenu: () => menu,
     };
     return MenuObject;
 };
-console.log(Object.keys(createMenu()));
+// console.log(typeof Object.values(createMenu()));
+// console.log(Object.keys(createMenu({ food: {}, drinks: {} }).fetchMenu()));
+const menu = createMenu({ food: {}, drinks: {} });
+// const keyMenu = Object.keys(menu.fetchMenu());
+// console.log(keyMenu);
+// console.log(createMenu({ food: {}, drinks: {} }).fetchMenu());
+const recoverMenu = menu.fetchMenu();
+console.log(recoverMenu);
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -3,7 +3,21 @@ const createMenu = require('../src/restaurant');
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
     // Escreva todos os testes aqui.
+    const menu = createMenu({ food: {}, drinks: {} });
     //verifica se a função createMenu() retorna um objeto que possui a chave fetchMenu
-    expect(Object.keys(createMenu())).toContain('fetchMenu');
+    expect(Object.keys(menu)).toContain('fetchMenu');
+
+    //verifica se o valor da chave fetchMenu do objeto retornado pela função createMenu() é uma função
+    expect(typeof menu.fetchMenu).toEqual('function');
+
+    // verifica se o objeto retornado pela função createMenu({ food: {}, drinks: {} }).fetchMenu() retorna um objeto cujas chaves são somente food e drinks.
+    const keysMenu = Object.keys(menu.fetchMenu());
+    expect(keysMenu).toEqual(['food', 'drinks']);
+    expect(keysMenu).not.toEqual(['food', 'drinks', 'reserv']);
+
+    //verifica se o menu passado pra função createMenu() é idêntico ao menu recuperado pela função createMenu({ food: {}, drinks: {} }).fetchMenu().
+    const passedMenu = { food: {}, drinks: {} };
+    const recoverMenu = menu.fetchMenu();
+    expect(passedMenu).toEqual(recoverMenu);
   });
 });
