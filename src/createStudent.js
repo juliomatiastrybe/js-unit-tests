a7e32746af58201f089cd8cbed96945d1c68c972
Julio Matias <juliocesark2@hotmail.com>
2023-08-20 00:40:41 -0300

    requesito 5 ok!

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nameStudent) => {
+  const feedback = () => 'Eita pessoa boa!';
+  return { name: nameStudent, feedback };
+};
 
+// const estudante = createStudent('Leandrão, o Lobo Solitário');
+// console.log(estudante.name);
+// console.log(estudante.feedback());
 module.exports = createStudent;
