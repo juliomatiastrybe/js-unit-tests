bcf6d0583d7bd2eab9bdec63c5273b12fe2cd3ea
Julio Matias <juliocesark2@hotmail.com>
2023-08-20 18:54:30 -0300

    requesito 10.1 ok

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,6 +1,11 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 
-const createMenu = () => {};
-
+const createMenu = () => {
+    const MenuObject = {
+        fetchMenu: () => {},
+    };
+    return MenuObject;
+};
+console.log(Object.keys(createMenu()));
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -2,7 +2,8 @@ const createMenu = require('../src/restaurant');
  
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // Escreva todos os testes aqui.
+    //verifica se a função createMenu() retorna um objeto que possui a chave fetchMenu
+    expect(Object.keys(createMenu())).toContain('fetchMenu');
   });
 });
