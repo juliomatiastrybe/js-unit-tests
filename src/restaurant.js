3852234c0bc901a81d8274951bb15e5b40a76550
Julio Matias <juliocesark2@hotmail.com>
2023-08-20 23:34:24 -0300

    requesito 10.8 ok

--- src/restaurant.js
+++ src/restaurant.js
@@ -2,19 +2,39 @@
 // Siga as orientações do README!
 
 const createMenu = (menu) => {
-    const MenuObject = {
+    const menuObject = {
         fetchMenu: () => menu,
         consumption: [],
+        order: (menuOrder) => {
+            const arrayMenu = Object.values(menuObject.fetchMenu());
+            const keysArray = Object.keys(arrayMenu[0]).concat(Object.keys(arrayMenu[1]));
+            
+            if (keysArray.includes(menuOrder)) {
+                menuObject.consumption.push(menuOrder);
+                // console.log(menuObject);
+            } else {
+                return 'Item indisponível';
+            }
+        },
     };
-    return MenuObject;
+    return menuObject;
 };
 // console.log(typeof Object.values(createMenu()));
 // console.log(Object.keys(createMenu({ food: {}, drinks: {} }).fetchMenu()));
 // const menu = createMenu({ food: {}, drinks: {} });
+// const menu = createMenu({ food: { coxinha: 3.90, sanduiche: 9.90 },
+//     drinks: { agua: 3.90, cerveja: 6.90 } });
 // console.log(menu);
 // const keyMenu = Object.keys(menu.fetchMenu());
 // console.log(keyMenu);
 // console.log(createMenu({ food: {}, drinks: {} }).fetchMenu());
 // const recoverMenu = menu.fetchMenu();
-// console.log(recoverMenu);
+// const foods = Object.values(recoverMenu);
+// console.log(foods);
+// console.log(Object.keys(foods[1]));
+// menu.order('coxinha');
+// console.log(menu);
+// menu.order('xablau');
+// console.log(menu.order('xablau'));
+// console.log(menu);
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -5,10 +5,15 @@ describe('10 - Implemente a função `createMenu`, bem como seus casos de teste'
     // Escreva todos os testes aqui.
     const menu = createMenu({ food: {}, drinks: {} });
 
-    //verifica se a função createMenu() retorna um objeto que possui a chave fetchMenu
+    const passedMenu = { food: {}, drinks: {} };
+
+    const passedMenu2 = { food: { coxinha: 3.90, sanduiche: 9.90 },
+    drinks: { agua: 3.90, cerveja: 6.90 } };
+
+    // verifica se a função createMenu() retorna um objeto que possui a chave fetchMenu
     expect(Object.keys(menu)).toContain('fetchMenu');
 
-    //verifica se o valor da chave fetchMenu do objeto retornado pela função createMenu() é uma função
+    // verifica se o valor da chave fetchMenu do objeto retornado pela função createMenu() é uma função
     expect(typeof menu.fetchMenu).toEqual('function');
 
     // verifica se o objeto retornado pela função createMenu({ food: {}, drinks: {} }).fetchMenu() retorna um objeto cujas chaves são somente food e drinks.
@@ -16,12 +21,23 @@ describe('10 - Implemente a função `createMenu`, bem como seus casos de teste'
     expect(keysMenu).toEqual(['food', 'drinks']);
     expect(keysMenu).not.toEqual(['food', 'drinks', 'reserv']);
 
-    //verifica se o menu passado pra função createMenu() é idêntico ao menu recuperado pela função createMenu({ food: {}, drinks: {} }).fetchMenu().
-    const passedMenu = { food: {}, drinks: {} };
+    // verifica se o menu passado pra função createMenu() é idêntico ao menu recuperado pela função createMenu({ food: {}, drinks: {} }).fetchMenu().
     const recoverMenu = menu.fetchMenu();
     expect(passedMenu).toEqual(recoverMenu);
 
-    //verifica se a propriedade consumption do objeto retornado pela função createMenu({ food: {}, drinks: {} }), após a criação do menu, retorna um array vazio.
+    // verifica se a propriedade consumption do objeto retornado pela função createMenu({ food: {}, drinks: {} }), após a criação do menu, retorna um array vazio.
     expect(menu.consumption).toEqual([]);
+
+    // verifica se o parâmetro passado do order, caso não conste no objeto passado como parâmetro para createMenu (nem em food ou drinks) retorne "Item indisponível".
+    const menu2 = createMenu(passedMenu2);
+    expect(menu2.order('mistão')).toBe('Item indisponível');
+
+    // verifica se o parâmetro passado do order, caso não conste no objeto passado como parâmetro para createMenu (nem em food ou drinks) não alterar a chave consumption.
+    expect(menu2.consumption).toEqual([]);
+
+    // verifica se caso o valor do parêmtro do order exista no objeto passado como parâmetro para createMenu o item deve ser adicionado ao array consumption.
+    menu2.order('sanduiche');
+    expect(menu2.consumption).toContain('sanduiche');
+
   });
 });
